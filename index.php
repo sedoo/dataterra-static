7aee4ddfb9871eefed8a3753995df5070522bdc3
Pierre Vert <vert@obs-mip.fr>
2019-06-20 12:08:40 +0200

    debug Ok

---
+++ index.php
@@ -0,0 +1,173 @@
+<?php
+include('header.php');
+?>
+
+<body>
+      <?php
+      include('menu.php');
+      ?>
+    <div id="fullpage">
+        <div class="section active" id="section0">
+            <div class="d-flex">
+                <div>
+                    <img src="asset/img/logo-dt.png" alt="logo Data Terra">
+                </div>
+                <div>
+                    <h1>Infrastructure de <span class="h2">recherche</span></h1>
+                    <p>Observer, comprendre et prévoir de manière intégrée l’histoire, le fonctionnement et l’évolution du système Terre soumis aux changements globaux est un enjeu fondamental de recherche et une nécessité pour de nombreuses applications environnementales et socio-économiques en lien avec la mise en œuvre des objectifs du développement durable. Cela nécessite des infrastructures interopérables permettant d’accélérer l’extraction, l’analyse, la diffusion et l’usage intelligent des données, indicateurs et modèles issus des systèmes nationaux et internationaux d’observation.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="section" id="section1">
+            <h2 class="text-center">Data Terra</h2>
+            <h3 class="text-center">En chiffres</h3>
+            <div class="d-flex">
+                <div class="chiffre text-center">
+                    <p class="big">7 000 to</p>
+                    <p class="text-center">de données stockées en 2017</p>
+                </div>
+                <div class="chiffre text-center">
+                    <p class="big">50 000 to</p>
+                    <p class="text-center">d'ici à 5 ans</p>
+                </div>
+                <div class="chiffre text-center">
+                    <p class="big">153,2</p>
+                    <p class="text-center">Équivalent temps plein travaillé</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="section" id="section2">
+            <div class="d-flex">
+                <div class="svg-container">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="680px" height="680px" viewBox="0 0 680 680" style="enable-background:new 0 0 680 680;" xml:space="preserve">
+                        <use xlink:href="#aeris"></use>
+                    </svg>
+                </div>
+                <div class="text">
+                    <h2>Atmosphere</h2>
+                    <h3>Pôle Aeris</h3>
+                    <p>Les recherches dans le domaine atmosphérique concernent des thématiques comme la dynamique, la physique et la chimie atmosphérique. Elles incluent aussi des travaux plus orientés vers l’étude de l’évolution du climat. Pour effectuer ces recherches, la communauté scientifique utilise des modèles mais aussi des observations qu’elles soient obtenues au sol, au moyen de satellites ou de véhicules aéroportés.<br>
+                        Un des enjeux de ce siècle est de rendre ces données accessibles à une large communauté, que cela soit pour des activités de recherche ou des applications commerciales. Ces données d’observations, pour être utiles, doivent être étalonnées, validées, homogénéisées. C’est dans ce contexte qu’intervient, AERIS.</p>
+                    <a href="https://www.aeris-data.fr/" target="_blank" class="btn">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="section" id="section3">
+            <div class="d-flex">
+                <div class="text">
+                    <h2>Terre<br>Solide</h2>
+                    <h3>Pôle Formater</h3>
+                    <p>Le pôle Terre solide a pour objectif de faciliter l’accès aux données et contribuer à la création de nouveaux produits et services en apportant de la valeur ajoutée aux données spatiales et « in-situ » disponibles. Il s’inscrit dans les paysages national et européen en articulation étroite avec les infrastructures en place et en construction.<br>
+                        Pour cela, le pôle Terre solide, a pour mission de fédérer les centres existants au service de la communauté Terre Solide.</p>
+                    <a href="https://www.poleterresolide.fr/" target="_blank" class="btn">En savoir plus</a>
+                </div>
+                <div class="svg-container">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="680px" height="680px" viewBox="0 0 680 680" style="enable-background:new 0 0 680 680;" xml:space="preserve">
+                        <use xlink:href="#formater"></use>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="section" id="section4">
+            <div class="d-flex">
+                <div class="svg-container">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="680px" height="680px" viewBox="0 0 680 680" style="enable-background:new 0 0 680 680;" xml:space="preserve">
+                        <use xlink:href="#odatis"></use>
+                    </svg>
+                </div>
+                <div class="text">
+                        <h2>Océano<br class="hidden-lg">graphie</h2>
+                        <h3>Pôle Odatis</h3>
+                        <p>
+                          Le pôle ODATIS (Données et Services pour l’Océan), point d’accès pour les données d’observation de l’Océan.<br>
+                          Il a pour objectif général de promouvoir et faciliter l’utilisation des observations réalisées dans l’océan ou à son interface avec les autres milieux, à partir de mesures in-situ et de télédétection.<br>
+                          ODATIS contribue à décrire, quantifier et comprendre l’océan dans sa globalité, hauturier et côtier, sur des thématiques comme : la dynamique et thermodynamique de l’océan; l’évolution de ses propriétés physico-chimiques; les cycles biogéochimiques; le fonctionnement des écosystèmes marins; l'évolution de l'océan et du lien océan-climat dans le passé; etc.
+                        </p>
+                        <a href="https://www.odatis-ocean.fr/" target="_blank" class="btn">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="section" id="section5">
+            <div class="d-flex">
+                <div class="text">
+                    <h2>Surface<br>Continentale</h2>
+                    <h3>Pôle Theia</h3>
+                    <p>Le pôle de données et de services surfaces continentales Theia a été créé fin 2012 par 9 institutions publiques françaises impliquées dans l’observation de la terre et les sciences de l’environnement. Cette structure scientifique et technique a pour vocation de faciliter l’usage des images issues de l’observation des surfaces continentales depuis l’espace.<br>
+                        Theia met à disposition de la communauté scientifique nationale et internationale, et des politiques publiques de suivi et de gestion des ressources environnementales, une vaste panoplie d’images à différentes échelles, de produits, de méthodes et de formation liés à l’observation des surfaces continentales en particulier depuis l’espace.</p>
+                    <a href="https://www.theia-land.fr/" target="_blank" class="btn">En savoir plus</a>
+                </div>
+                <div class="svg-container">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="680px" height="680px" viewBox="0 0 680 680" style="enable-background:new 0 0 680 680;" xml:space="preserve">
+                        <use xlink:href="#theia"></use>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="section" id="section6">
+            <h2 class="text-center">Nos partenaires</h2>
+            <ul class="d-flex">
+                <li><a href="https://cnes.fr/fr" target="_blank"><img src="asset/img/logo-01.svg" alt="logo CNES"/></a></li> 
+                <li><a href="http://www.cnrs.fr/fr/page-daccueil" target="_blank"><img src="asset/img/logo-02.svg" alt="logo CNRS"/></a></li> 
+                <li><a href="" target="_blank"><img src="asset/img/logo-03.svg" alt="logo Ifremer"/></a></li> 
+                <li><a href="" target="_blank"><img src="asset/img/logo-04.svg" alt="logo IGN"/></a></li>
+            </ul>
+            <ul class="d-flex">
+                <li><a href="" target="_blank"><img src="asset/img/logos-05.png" alt="logo IRD"/></a></li> 
+                <li><a href="" target="_blank"><img src="asset/img/logos-06.png" alt="logo IRSTEA"/></a></li> 
+                <li><a href="" target="_blank"><img src="asset/img/logos-07.png" alt="logo MétéoFrance"/></a></li> 
+            </ul>
+        </div>
+    </div>
+
+<?php
+include('asset/svg/dataterra.svg');
+?>
+
+    <script type="text/javascript" src="asset/js/fullpage.min.js"></script>
+    <script type="text/javascript">
+        var myFullpage = new fullpage('#fullpage', {
+            // verticalCentered: false,
+            anchors: ['firstSectionAnchor', 'chiffresAnchor', 'aerisAnchor', 'formaterAnchor', 'odatisAnchor', 'theiaAnchor', 'partenairesAnchor'],
+            menu: '#menu',
+            lazyLoad: true,
+            //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
+            // css3: false,
+//            navigation: true,
+//            slidesNavigation: true,
+        });
+    </script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
+    <script>
+      //   $(document).ready(function() {
+//                $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+//                var page = $(this).attr('href'); // Page cible
+//                var speed = 750; // Durée de l'animation (en ms)
+//                $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+//                return false;
+//            });
+            
+      //       $(".burger").click(function () {
+      //           $(".navbar ul").toggleClass("active");
+      //           $(".burger").toggleClass("active");
+
+      //           var delay = 0;
+      //           $('.navbar ul li').each(function () {
+      //               var $li = $(this);
+
+      //               setTimeout(function () {
+      //                   $li.toggleClass('transition-open');
+      //               }, delay += 100); // delay 100 ms
+      //           });
+
+      //       });
+      //   });
+    </script>
+</body>
+
+</html>
